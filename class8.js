e1b148604b659e47ce63b7cf5dc29e7c3f7c43f4
Muhammad Adil <141252873+adil273@users.noreply.github.com>
2023-09-03 10:12:09 -0700

    Add files via upload

---
+++ class8.js
@@ -0,0 +1,12 @@
+"use strict";
+let cars = [{
+        brand: "Honda",
+        year: 2009,
+        reg: "Lahore",
+    },
+    {
+        brand: "Toyota",
+        year: 2010,
+        reg: "Lahore",
+    }];
+console.log(cars);
---
+++ class8.ts
@@ -0,0 +1,19 @@
+type cartype = {
+    brand:string;
+    year:number;
+    reg:string;
+
+}
+let cars:cartype[]=[{
+    brand:"Honda",
+    year:2009,
+    reg:"Lahore",
+
+},
+{
+    brand:"Toyota",
+    year:2010,
+    reg:"Lahore",
+    
+}]
+console.log(cars);
